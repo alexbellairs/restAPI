a32f60f2530959b7274b3aa35396ac403184fdb0
alexbellairs <alexanderbellairs@gmail.com>
2022-07-13 11:45:56 +0100

    ref

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require("./db/connection"); //instantly run database connection
 const express = require("express"); //pull in all of express module
+const cors = require("cors");
 const userRouter = require("./user/routes"); //bring in all endpoints connected to userRouter
 const app = express(); //create webserver constant to manipulate
 const port = process.env.PORT || 5001; //store either supplied port or 5001
 
 app.use(express.json()); //parses all requests as if they are JSON, sends all responses as JSON
 
+app.use(cors());
 app.use(userRouter);
 
 app.listen(port, () => {
